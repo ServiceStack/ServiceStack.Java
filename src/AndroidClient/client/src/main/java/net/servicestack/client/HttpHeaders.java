022daadfceb55d5aeda7ae87529fc898a25d823e
Demis Bellot <demis.bellot@gmail.com>
2015-03-25 01:00:13 -0400

    Extract HttpHeaders, HttpMethods and MimeTypes into their own classes

---
+++ src/AndroidClient/client/src/main/java/net/servicestack/client/HttpHeaders.java
@@ -0,0 +1,9 @@
+//  Copyright (c) 2015 ServiceStack LLC. All rights reserved.
+
+package net.servicestack.client;
+
+public class HttpHeaders {
+    public static final String Accept = "Accept";
+    public static final String ContentType = "Content-Type";
+    public static final String ContentLength = "Content-Length";
+}
---
+++ src/AndroidClient/client/src/main/java/net/servicestack/client/HttpMethods.java
@@ -0,0 +1,14 @@
+//  Copyright (c) 2015 ServiceStack LLC. All rights reserved.
+
+package net.servicestack.client;
+
+public class HttpMethods {
+    public static final String Options = "OPTIONS";
+    public static final String Head = "HEAD";
+    public static final String Get = "GET";
+    public static final String Post = "POST";
+    public static final String Put = "PUT";
+    public static final String Delete = "DELETE";
+    public static final String Trace = "TRACE";
+    public static final String Patch = "PATCH";
+}
