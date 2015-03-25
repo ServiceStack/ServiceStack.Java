8b9b35d5f348bd0fa1cdd4966b6d9b559e8e5017
Demis Bellot <demis.bellot@gmail.com>
2015-03-25 00:59:29 -0400

    Add types with dependencies on Android in different .android package

---
+++ src/AndroidClient/client/src/main/java/net/servicestack/android/AndroidLogProvider.java
@@ -0,0 +1,37 @@
+package net.servicestack.android;
+
+import net.servicestack.client.LogProvider;
+import net.servicestack.client.LogType;
+
+public class AndroidLogProvider extends LogProvider {
+
+    public AndroidLogProvider(){
+        super(null, true);
+    }
+
+    public AndroidLogProvider(String prefix) {
+        super(prefix, true);
+    }
+
+    public AndroidLogProvider(String prefix, boolean debugEnabled) {
+        super(prefix, debugEnabled);
+    }
+
+    @Override
+    public void println(LogType type, Object message) {
+        switch (type){
+            case DEBUG:
+                android.util.Log.d(getPrefix(), message.toString());
+                break;
+            case INFO:
+                android.util.Log.i(getPrefix(), message.toString());
+                break;
+            case WARN:
+                android.util.Log.w(getPrefix(), message.toString());
+                break;
+            case ERROR:
+                android.util.Log.e(getPrefix(), message.toString());
+                break;
+        }
+    }
+}
---
+++ src/AndroidClient/client/src/main/java/net/servicestack/android/AndroidServiceClient.java
@@ -0,0 +1,12 @@
+package net.servicestack.android;
+
+import net.servicestack.client.JsonServiceClient;
+
+public class AndroidServiceClient extends JsonServiceClient {
+
+    public AndroidServiceClient(String baseUrl) {
+        super(baseUrl);
+    }
+
+
+}
