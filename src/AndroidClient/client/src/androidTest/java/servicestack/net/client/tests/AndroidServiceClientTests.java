407e88dcc34a09d1cb43f9eac28e471946966552
Demis Bellot <demis.bellot@gmail.com>
2015-03-28 05:57:36 -0400

    Add Async ServiceClient tests

---
+++ src/AndroidClient/client/src/androidTest/java/servicestack/net/client/tests/AndroidServiceClientTests.java
@@ -0,0 +1,21 @@
+package servicestack.net.client.tests;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+import net.servicestack.android.AndroidLogProvider;
+import net.servicestack.android.AndroidServiceClient;
+import net.servicestack.client.Log;
+
+public class AndroidServiceClientTests extends ApplicationTestCase<Application> {
+    public AndroidServiceClientTests() {
+        super(Application.class);
+        Log.Instance = new AndroidLogProvider("ZZZ");
+    }
+
+    AndroidServiceClient client = new AndroidServiceClient("http://techstacks.io");
+
+    public void test(){
+
+    }
+}
---
+++ src/AndroidClient/client/src/androidTest/java/servicestack/net/client/tests/TechStacksServiceTestsAsync.java
@@ -0,0 +1,181 @@
+package servicestack.net.client.tests;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+import net.servicestack.android.AndroidLogProvider;
+import net.servicestack.android.AndroidServiceClient;
+import net.servicestack.client.AsyncResponse;
+import net.servicestack.client.Log;
+import net.servicestack.client.Utils;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import io.techstacks.dto.*;
+
+public class TechStacksServiceTestsAsync extends ApplicationTestCase<Application> {
+    public TechStacksServiceTestsAsync() {
+        super(Application.class);
+        Log.Instance = new AndroidLogProvider("ZZZ");
+    }
+
+    AndroidServiceClient client = new AndroidServiceClient("http://techstacks.io");
+
+    public void test_Can_GET_TechStacks_Overview() throws InterruptedException {
+        final CountDownLatch signal = new CountDownLatch(1);
+
+        client.getAsync(new Overview(), new AsyncResponse<OverviewResponse>(){
+            @Override
+            public void success(OverviewResponse response) {
+                TechStacksServiceTests.assertOverviewResponse(response);
+            }
+
+            @Override
+            public void complete() {
+                signal.countDown();
+            }
+        });
+
+        assertTrue(signal.await(5, TimeUnit.SECONDS));
+    }
+
+    public void test_Can_GET_TechStacks_AppOverview_Async() throws InterruptedException {
+        final CountDownLatch signal = new CountDownLatch(1);
+
+        client.getAsync(new AppOverview(), new AsyncResponse<AppOverviewResponse>(){
+            @Override
+            public void success(AppOverviewResponse r) {
+                assertNotNull(r);
+                assertTrue(r.getTopTechnologies().size() > 0);
+                assertTrue(r.getAllTiers().size() > 0);
+            }
+
+            @Override
+            public void complete() {
+                signal.countDown();
+            }
+        });
+
+        assertTrue(signal.await(5, TimeUnit.SECONDS));
+    }
+
+    public void test_Can_GET_TechStacks_Overview_with_relative_url_Async() throws InterruptedException {
+        final CountDownLatch signal = new CountDownLatch(1);
+
+        client.getAsync("/overview", OverviewResponse.class, new AsyncResponse<OverviewResponse>() {
+            @Override
+            public void success(OverviewResponse response) {
+                TechStacksServiceTests.assertOverviewResponse(response);
+            }
+
+            @Override
+            public void complete() {
+                signal.countDown();
+            }
+        });
+
+        assertTrue(signal.await(5, TimeUnit.SECONDS));
+    }
+
+    public void test_Can_GET_TechStacks_Overview_with_absolute_url_Async() throws InterruptedException {
+        final CountDownLatch signal = new CountDownLatch(1);
+
+        client.getAsync("http://techstacks.io/overview", OverviewResponse.class, new AsyncResponse<OverviewResponse>() {
+            @Override
+            public void success(OverviewResponse response) {
+                TechStacksServiceTests.assertOverviewResponse(response);
+            }
+
+            @Override
+            public void complete() {
+                signal.countDown();
+            }
+        });
+
+        assertTrue(signal.await(5, TimeUnit.SECONDS));
+    }
+
+    public void test_Can_GET_GetTechnology_with_params_Async() throws InterruptedException {
+        GetTechnology requestDto = new GetTechnology()
+            .setSlug("servicestack");
+
+        final CountDownLatch signal = new CountDownLatch(1);
+
+        client.getAsync(requestDto, new AsyncResponse<GetTechnologyResponse>() {
+            @Override
+            public void success(GetTechnologyResponse response) {
+                TechStacksServiceTests.assertGetTechnologyResponse(response);
+            }
+
+            @Override
+            public void complete() {
+                signal.countDown();
+            }
+        });
+
+        assertTrue(signal.await(5, TimeUnit.SECONDS));
+    }
+
+    public void test_Can_GET_GetTechnology_with_url_Async() throws InterruptedException {
+        final CountDownLatch signal = new CountDownLatch(1);
+
+        client.getAsync("/technology/servicestack", GetTechnologyResponse.class, new AsyncResponse<GetTechnologyResponse>() {
+            @Override
+            public void success(GetTechnologyResponse response) {
+                TechStacksServiceTests.assertGetTechnologyResponse(response);
+            }
+
+            @Override
+            public void complete() {
+                signal.countDown();
+            }
+        });
+
+        assertTrue(signal.await(5, TimeUnit.SECONDS));
+    }
+
+    public void test_Can_call_FindTechnologies_AutoQuery_Service_Async() throws InterruptedException {
+        FindTechnologies request = new FindTechnologies()
+            .setName("ServiceStack");
+
+        final CountDownLatch signal = new CountDownLatch(1);
+
+        client.getAsync(request, new AsyncResponse<QueryResponse<Technology>>() {
+            @Override
+            public void success(QueryResponse<Technology> response) {
+                assertEquals(1, response.getResults().size());
+
+            }
+
+            @Override
+            public void complete() {
+                signal.countDown();
+            }
+        });
+
+        assertTrue(signal.await(5, TimeUnit.SECONDS));
+    }
+
+    public void test_Can_call_FindTechnologies_AutoQuery_Implicit_Service() throws InterruptedException {
+        FindTechnologies request = (FindTechnologies) new FindTechnologies()
+            .setTake(5);
+
+        final CountDownLatch signal = new CountDownLatch(1);
+
+        client.getAsync(request, Utils.createMap("DescriptionContains", "framework"),
+            new AsyncResponse<QueryResponse<Technology>>() {
+                @Override
+                public void success(QueryResponse<Technology> response) {
+                    assertEquals(5, response.getResults().size());
+                }
+
+                @Override
+                public void complete() {
+                    signal.countDown();
+                }
+            });
+
+        assertTrue(signal.await(5, TimeUnit.SECONDS));
+    }
+}
