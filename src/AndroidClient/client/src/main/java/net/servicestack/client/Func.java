9f488df979aae5dca00a871cc005cf0088c81b64
Demis Bellot <demis.bellot@gmail.com>
2015-04-11 13:40:42 -0400

    Add new Func utils + treat nulls as empty collections

--- src/AndroidClient/client/src/main/java/net/servicestack/client/Func.java
+++ src/AndroidClient/client/src/main/java/net/servicestack/client/Func.java
@@ -7,6 +7,10 @@ import java.util.Collections;
 import java.util.Iterator;
 
 public class Func {
+    public static interface Each<T> {
+        public void apply(T t);
+    }
+
     public static interface Function<T,R> {
         public R apply(T t);
     }
@@ -17,6 +21,8 @@ public class Func {
 
     public static <T,R> ArrayList<R> map(Iterable<T> xs, Function<T,R> f) {
         ArrayList<R> to = new ArrayList<>();
+        if (xs == null) return to;
+
         for (T x : xs) {
             R ret = f.apply(x);
             to.add(ret);
@@ -24,8 +30,33 @@ public class Func {
         return to;
     }
 
+    public static <T> void each(Iterable<T> xs, Each<T> f) {
+        if (xs == null) return;
+        for (T x : xs) {
+            f.apply(x);
+        }
+    }
+
+    public static ArrayList<Integer> toList(int... xs){
+        ArrayList<Integer> to = new ArrayList<>();
+        for (int x : xs) {
+            to.add(x);
+        }
+        return to;
+    }
+
+    public static <T> ArrayList<T> toList(T... xs){
+        ArrayList<T> to = new ArrayList<>();
+        for (T x : xs) {
+            to.add(x);
+        }
+        return to;
+    }
+
     public static <T> ArrayList<T> toList(Iterable<T> xs){
         ArrayList<T> to = new ArrayList<>();
+        if (xs == null) return to;
+
         for (T x : xs) {
             to.add(x);
         }
@@ -34,6 +65,8 @@ public class Func {
 
     public static <T> ArrayList<T> filter(Iterable<T> xs, Predicate<T> predicate){
         ArrayList<T> to = new ArrayList<>();
+        if (xs == null) return to;
+
         for (T x : xs) {
             if (predicate.apply(x)){
                 to.add(x);
@@ -43,6 +76,8 @@ public class Func {
     }
 
     public static <T> T first(Iterable<T> xs, Predicate<T> predicate){
+        if (xs == null) return null;
+
         for (T x : xs) {
             if (predicate.apply(x)){
                 return x;
@@ -51,13 +86,42 @@ public class Func {
         return null;
     }
 
+    public static <T> T first(Iterable<T> xs){
+        if (xs == null) return null;
+
+        for (T x : xs) {
+            return x;
+        }
+        return null;
+    }
+
+    public static <T> T first(T[] xs) {
+        return xs == null || xs.length == 0 ? null : xs[0];
+    }
+
     public static <T> T last(Iterable<T> xs, Predicate<T> predicate){
+        if (xs == null) return null;
+
         for (T x : reverse(xs)) {
             if (predicate.apply(x)){
                 return x;
             }
         }
         return null;
+    }
+
+    public static <T> T last(Iterable<T> xs){
+        if (xs == null) return null;
+
+        T last = null;
+        for (T x : xs) {
+            last = x;
+        }
+        return last;
+    }
+
+    public static <T> T last(T[] xs) {
+        return xs == null ? null : xs[xs.length - 1];
     }
 
     public static <T> boolean contains(Iterable<T> xs, Predicate<T> predicate){
@@ -67,6 +131,8 @@ public class Func {
     public static <T> ArrayList<T> skip(Iterable<T> xs, int skip){
         int i = 0;
         ArrayList<T> to = new ArrayList<>();
+        if (xs == null) return to;
+
         for (T x : xs) {
             if (i++ >= skip){
                 to.add(x);
@@ -77,6 +143,8 @@ public class Func {
 
     public static <T> ArrayList<T> skip(Iterable<T> xs, Predicate<T> predicate){
         ArrayList<T> to = new ArrayList<>();
+        if (xs == null) return to;
+
         for (T x : xs) {
             if (predicate.apply(x)){
                 to.add(x);
@@ -87,6 +155,8 @@ public class Func {
 
     public static <T> ArrayList<T> take(Iterable<T> xs, Predicate<T> predicate){
         ArrayList<T> to = new ArrayList<>();
+        if (xs == null) return to;
+
         for (T x : xs) {
             if (predicate.apply(x)){
                 return to;
@@ -99,6 +169,8 @@ public class Func {
     public static <T> ArrayList<T> take(Iterable<T> xs, int take){
         int i = 0;
         ArrayList<T> to = new ArrayList<>();
+        if (xs == null) return to;
+
         for (T x : xs) {
             if (i++ >= take){
                 return to;
@@ -109,6 +181,8 @@ public class Func {
     }
 
     public static <T> boolean any(Iterable<T> xs, Predicate<T> predicate){
+        if (xs == null) return false;
+
         for (T x : xs) {
             if (predicate.apply(x)){
                 return true;
@@ -118,6 +192,8 @@ public class Func {
     }
 
     public static <T> boolean all(Iterable<T> xs, Predicate<T> predicate){
+        if (xs == null) return false;
+
         for (T x : xs) {
             if (!predicate.apply(x)){
                 return false;
@@ -128,6 +204,8 @@ public class Func {
 
     public static <T> ArrayList<T> expand(Iterable<T>... xss){
         ArrayList<T> to = new ArrayList<>();
+        if (xss == null) return to;
+
         for (Iterable<T> xs : xss) {
             for (T x : xs){
                 to.add(x);
@@ -137,6 +215,8 @@ public class Func {
     }
 
     public static <T> T elementAt(Iterable<T> xs, int index){
+        if (xs == null) return null;
+
         int i = 0;
         for (T x : xs){
             if (i++ == index){
@@ -147,12 +227,16 @@ public class Func {
     }
 
     public static <T> ArrayList<T> reverse(Iterable<T> xs){
+        if (xs == null) return new ArrayList<T>();
+
         ArrayList<T> clone = toList(xs);
         Collections.reverse(clone);
         return clone;
     }
 
     public static <T,E> E reduce(Iterable<T> xs, E initialValue, Reducer<T,E> reducer){
+        if (xs == null) return initialValue;
+
         E currentValue = initialValue;
         for (T x : xs){
             currentValue = reducer.reduce(currentValue, x);
@@ -166,6 +250,8 @@ public class Func {
 
     public static <T> String join(Iterable<T> xs, String separator){
         StringBuilder sb = new StringBuilder();
+        if (xs == null) return sb.toString();
+
         for (T x : xs){
             if (sb.length() > 0)
                 sb.append(separator);
