f35d0cdcf8bfc4ed8cba914c187cdb37aedcefe8
Demis Bellot <demis.bellot@gmail.com>
2015-04-18 12:22:04 -0400

    Add raw image bytes sync and async AndroidServiceClient tests

--- src/AndroidClient/android/src/androidTest/java/net/servicestack/android/ApplicationTest.java
+++ src/AndroidClient/android/src/androidTest/java/net/servicestack/android/ApplicationTest.java
@@ -1,7 +1,16 @@
 package net.servicestack.android;
 
 import android.app.Application;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.test.ApplicationTestCase;
+
+import net.servicestack.client.AsyncResult;
+import net.servicestack.client.Utils;
+
+import java.net.HttpURLConnection;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
@@ -9,5 +18,37 @@ import android.test.ApplicationTestCase;
 public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
+    }
+
+    AndroidServiceClient client = new AndroidServiceClient("http://techstacks.io");
+
+    public void test_Can_download_image_bytes(){
+        HttpURLConnection httpRes = client.get("https://servicestack.net/img/logo.png");
+        byte[] imgBytes = Utils.readBytesToEnd(httpRes);
+        Bitmap img = BitmapFactory.decodeByteArray(imgBytes, 0, imgBytes.length);
+
+        assertEquals(338, img.getWidth());
+        assertEquals(55, img.getHeight());
+    }
+
+    public void test_Can_download_image_bytes_Async() throws InterruptedException {
+        final CountDownLatch signal = new CountDownLatch(1);
+
+        client.getAsync("https://servicestack.net/img/logo.png", new AsyncResult<byte[]>() {
+            @Override
+            public void success(byte[] imgBytes) {
+                Bitmap img = BitmapFactory.decodeByteArray(imgBytes, 0, imgBytes.length);
+
+                assertEquals(338, img.getWidth());
+                assertEquals(55, img.getHeight());
+            }
+
+            @Override
+            public void complete() {
+                signal.countDown();
+            }
+        });
+
+        assertTrue(signal.await(5, TimeUnit.SECONDS));
     }
 }
