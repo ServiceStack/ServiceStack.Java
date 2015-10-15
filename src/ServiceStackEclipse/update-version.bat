d50c84e1c076cef7aa8b63ca7d5849adbcd5794b
Darren Reid <darren.j.reid@gmail.com>
2015-10-15 18:41:50 +1100

    Update script + testing when updating version info

--- src/ServiceStackEclipse/update-version.bat
+++ src/ServiceStackEclipse/update-version.bat
@@ -2,5 +2,5 @@ echo off
 if "%1" == "" (
 echo Provide a version number in the form x.x.x
 ) else (
-mvn tycho-versions:set-version -DnewVersion=%1 -Dartifacts=net.servicestack.eclipse,ServiceStackEclipse.Repository,net.servicestack.eclipse.feature
+mvn tycho-versions:set-version -DnewVersion=%1 -Dartifacts=net.servicestack.eclipse,ServiceStackEclipse.Repository,net.servicestack.eclipse.feature,testing
 )
