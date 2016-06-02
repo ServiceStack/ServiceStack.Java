4df3d79111d398e3381ba9b6ed598288c4b892b8
Darren Reid <darren.j.reid@gmail.com>
2016-06-02 11:23:48 +1000

    Use older Java syntax to support 1.7 gradle build

--- src/ServiceStackIDEA/src/main/java/net/servicestack/idea/Analytics.java
+++ src/ServiceStackIDEA/src/main/java/net/servicestack/idea/Analytics.java
@@ -30,17 +30,20 @@ public final class Analytics {
             final URL[] serviceUrl = {null};
             final URLConnection[] responseConnection = {null};
             final BufferedReader[] responseReader = {null};
-            Thread thread = new Thread(() -> {
-                try {
-                    serviceUrl[0] = new URL(url);
-                    responseConnection[0] = serviceUrl[0].openConnection();
-                    responseReader[0] = new BufferedReader(
-                            new InputStreamReader(
-                                    responseConnection[0].getInputStream()));
+            Thread thread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        serviceUrl[0] = new URL(url);
+                        responseConnection[0] = serviceUrl[0].openConnection();
+                        responseReader[0] = new BufferedReader(
+                                new InputStreamReader(
+                                        responseConnection[0].getInputStream()));
 
-                    responseReader[0].close();
-                } catch (IOException e) {
-                    // Ignore failure (eg no internet connection).
+                        responseReader[0].close();
+                    } catch (IOException e) {
+                        // Ignore failure (eg no internet connection).
+                    }
                 }
             });
             thread.start();
