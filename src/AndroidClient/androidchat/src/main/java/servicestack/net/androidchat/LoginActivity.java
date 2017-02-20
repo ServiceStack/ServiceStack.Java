a7d101d7fbbeb7a3db20bba2ed4d1f89f2676809
Demis Bellot <demis.bellot@gmail.com>
2017-02-19 22:27:48 -0500

    Replace ServiceStack Xamarin.Auth with native Twitter Auth Client

--- src/AndroidClient/androidchat/src/main/java/servicestack/net/androidchat/LoginActivity.java
+++ src/AndroidClient/androidchat/src/main/java/servicestack/net/androidchat/LoginActivity.java
@@ -5,7 +5,6 @@ import android.animation.ObjectAnimator;
 import android.animation.ValueAnimator;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v4.view.animation.FastOutLinearInInterpolator;
@@ -15,9 +14,18 @@ import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ProgressBar;
 
-import net.servicestack.android.AndroidServiceClient;
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.Result;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.TwitterSession;
+import com.twitter.sdk.android.core.identity.TwitterAuthClient;
 
-import static servicestack.net.androidchat.dtos.GetUserDetails;
+import net.servicestack.android.AndroidServiceClient;
+import net.servicestack.client.Log;
+
+import io.fabric.sdk.android.Fabric;
 
 
 /**
@@ -27,6 +35,12 @@ import static servicestack.net.androidchat.dtos.GetUserDetails;
 public class LoginActivity extends AppCompatActivity {
     private ProgressBar progressBar;
     private ObjectAnimator animation;
+
+//  For: http://localhost:11001/
+    private static final String TWITTER_KEY = "3H1FHjGbA1N0n0aT5yApA";
+    private static final String TWITTER_SECRET = "MLrZ0ujK6DwyjlRk2YLp6HwSdoBjtuqwXeHDQLv0Q";
+
+    private TwitterAuthClient twitterAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -51,41 +65,55 @@ public class LoginActivity extends AppCompatActivity {
 
         ImageButton btnTwitter = (ImageButton)findViewById(R.id.btnTwitter);
         ImageButton btnAnon = (ImageButton)findViewById(R.id.btnAnon);
-        AndroidServiceClient client = new AndroidServiceClient(MainActivity.BaseUrl);
+        LoginActivity activity = this;
 
-//        btnTwitter.setOnClickListener(view -> {
-//            startProgressBar();
-//            Account existingAccount = tryResolveAccount();
-//            // If cookies saved from twitter login, automatically continue to chat activity.
-//            if (existingAccount != null){
-//                try {
-////                    client.CookieContainer = existingAccount.Cookies;
-//                    client.getAsync(new GetUserDetails(), res -> {
-//                        startAuthChatActivity(client, existingAccount);
-//                        stopProgressBar();
-//                    }, e -> {
-//                        // Failed with current cookie
-//                        client.clearCookies();
-//                        performServiceStackAuth(client);
-//                        stopProgressBar();
-//                    });
-//                } catch (Exception e){
-//                    // Failed with current cookie
-//                    client.clearCookies();
-//                    stopProgressBar();
-//                    performServiceStackAuth(client);
-//                }
-//            } else {
-//                stopProgressBar();
-//                performServiceStackAuth(client);
-//            }
-//        });
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+        Fabric.with(this, new Twitter(authConfig));
+
+        twitterAuth = new TwitterAuthClient();
+        btnTwitter.setOnClickListener(view -> {
+            startProgressBar();
+            twitterAuth.authorize(activity, new Callback<TwitterSession>() {
+                @Override
+                public void success(Result<TwitterSession> result) {
+                    TwitterSession session = result.data;
+
+                    App.get().getServiceClient().postAsync(new dtos.Authenticate()
+                            .setProvider("twitter")
+                            .setUserName(Long.toString(session.getUserId()))
+                            .setAccessToken(session.getAuthToken().token)
+                            .setAccessTokenSecret(session.getAuthToken().secret)
+                            .setRememberMe(true),
+                        r -> {
+                            Intent intent = new Intent(activity, MainActivity.class);
+                            startActivity(intent);
+                        },
+                        error -> {
+                            Log.e("TwitterAuthClient FAILED!", error);
+                        });
+
+                    stopProgressBar();
+                }
+
+                @Override
+                public void failure(TwitterException exception) {
+                    exception.printStackTrace();
+                    stopProgressBar();
+                }
+            });
+        });
 
         btnAnon.setOnClickListener(view -> {
             startProgressBar();
-            startGuestChatActivity(client);
+            startGuestChatActivity(App.get().getServiceClient());
             stopProgressBar();
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        twitterAuth.onActivityResult(requestCode, resultCode, data);
     }
 
     private void startProgressBar(){
