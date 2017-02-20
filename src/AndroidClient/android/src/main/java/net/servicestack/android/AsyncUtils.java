c5d1f886ab5c3384c15d8c2aa4373997e3c1ff85
Demis Bellot <demis.bellot@gmail.com>
2017-02-20 01:32:27 -0500

    Fix AsyncUtils.readBitmap

--- src/AndroidClient/android/src/main/java/net/servicestack/android/AsyncUtils.java
+++ src/AndroidClient/android/src/main/java/net/servicestack/android/AsyncUtils.java
@@ -8,6 +8,7 @@ import net.servicestack.client.AsyncResult;
 import net.servicestack.client.AsyncResultVoid;
 import net.servicestack.client.AsyncSuccess;
 import net.servicestack.client.AsyncSuccessVoid;
+import net.servicestack.client.Utils;
 
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -54,15 +55,32 @@ public class AsyncUtils {
         readBitmap(url, createAsyncResult(success, error));
     }
 
-    private static void readBitmap(final String url, final AsyncResult<Bitmap> asyncResult){
+    private static void readBitmap(final String imgUrl, final AsyncResult<Bitmap> asyncResult){
         new AsyncTask<String, Void, Bitmap>(){
 
             @Override
-            protected Bitmap doInBackground(String... url) {
+            protected Bitmap doInBackground(String... urls) {
                 HttpURLConnection conn = null;
+                String url = urls[0];
                 try {
-                    conn = (HttpURLConnection)new URL(url[0]).openConnection();
-                    return AndroidUtils.readBitmap(conn.getInputStream());
+                    while (!Utils.isNullOrEmpty(url)){
+                        conn = (HttpURLConnection)new URL(url).openConnection();
+                        conn.setInstanceFollowRedirects(true);
+
+                        switch (conn.getResponseCode()){
+                            case HttpURLConnection.HTTP_MOVED_PERM:
+                            case HttpURLConnection.HTTP_MOVED_TEMP:
+                                url = conn.getHeaderField("Location");
+                                if (!Utils.isNullOrEmpty(url))
+                                    continue;
+                        }
+
+                        Bitmap ret = AndroidUtils.readBitmap(conn.getInputStream());
+                        return ret;
+                    }
+
+                    asyncResult.setError(new RuntimeException("Could not download Bitmap from: " + urls[0]));
+                    return null;
                 } catch (Exception e) {
                     asyncResult.setError(e);
                     return null;
@@ -71,8 +89,8 @@ public class AsyncUtils {
 
             @Override
             protected void onPostExecute(Bitmap bitmap) {
-                asyncResult.setResult(bitmap);
+                asyncResult.completeResult(bitmap);
             }
-        };
+        }.execute(imgUrl);
     }
 }
