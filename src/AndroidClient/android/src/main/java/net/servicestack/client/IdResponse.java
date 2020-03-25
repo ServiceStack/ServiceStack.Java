5cb40d3ddd0a8aa03a734b5ffc6dfe9fbbccb05e
Demis Bellot <demis.bellot@gmail.com>
2020-03-25 22:53:00 +0800

    Add new built-in AutoCrud types

---
+++ src/AndroidClient/android/src/main/java/net/servicestack/client/IdResponse.java
@@ -0,0 +1,23 @@
+//  Copyright (c) 2015 ServiceStack LLC. All rights reserved.
+//  License: https://servicestack.net/bsd-license.txt
+
+package net.servicestack.client;
+
+@DataContract
+public class IdResponse {
+
+    @DataMember(Order=1)
+    public String id = null;
+
+    @DataMember(Order=2)
+    public ResponseStatus responseStatus = null;
+
+    public String getId() { return id; }
+    public IdResponse setId(String id) {
+        this.id = id; return this;
+    }
+    public ResponseStatus getResponseStatus() { return responseStatus; }
+    public IdResponse setResponseStatus(ResponseStatus responseStatus) {
+        this.responseStatus = responseStatus; return this;
+    }
+}
